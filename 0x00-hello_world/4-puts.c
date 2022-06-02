0768dc13b59d718b04ea94fe3f19669b5ddbf3bc
Nomynameisjames <adavaonimisi@gmail.com>
2022-06-02 13:27:09 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
- * main - main block
- * return: 0
+ * main - program prints with function puts
+ * return: always 0
  */
 int main(void)
 {
-	puts("programming is like building a multilingual puzzle");
+	puts("\"programming is like building a multilingual puzzle");
 	return(0);
 }
