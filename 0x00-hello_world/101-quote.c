603fbce2d5ff37bba80f4c8866a2c503061ca075
Nomynameisjames <adavaonimisi@gmail.com>
2022-06-02 20:15:28 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  * main - Entry point
- * Return:1
+ * Return: Always 1(success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\"
-	   - Dora Korpar, 2015-10-19\n", stdout);
+	char str1[] = "and that piece of art is useful\" - Dora Korpar,
+	     2015-1019\n";
+	write(2, str1, 59);
 	return (1);
 }
