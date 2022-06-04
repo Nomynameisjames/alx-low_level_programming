f1ea102c123d09eddef1eb1facc20717723c4834
Nomynameisjames <adavaonimisi@gmail.com>
2022-06-03 20:12:57 -0700

    task 3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet
+ * description: random number in n +ve/ -ve
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int firstLetter, n;
+
+for (firstLetter = 97,	n = 1; n < 27; firstLetter++,
+	n++);
+{
+putchar(firstLetter);
+}
+putchar(10);
+return (0);
+}
