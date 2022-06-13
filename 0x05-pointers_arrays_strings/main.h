1700c2fc31933064f1d72189b82b983f76bf9ad3
Nomynameisjames <adavaonimisi@gmail.com>
2022-06-13 15:23:37 -0700

    main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * void prototypes()
+ * char prototypes()
+ * int prototypes()
+ */
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -11,9 +16,8 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-char *_strcat(char *dest, char *src);
 int _atoi(char *s);
+int _putchar(char c);
 void swap_char(char *a, char *b);
 
-	;
-#endif /*MAIN.H*/
+#endif /*MAIN_H*/
