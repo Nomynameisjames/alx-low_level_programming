f4dceb2a5e94c43bece22394b1122cd68dd29f39
Nomynameisjames <adavaonimisi@gmail.com>
2022-06-13 17:35:35 -0700

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_array - prints a string in reverse
@@ -9,13 +10,17 @@ void print_array(int *a, int n)
 {
 	int i = 0;
 
-	for (; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
-		printf("%d", *(a + i));
-	}
-	if (i != (n - 1))
-	{
-		printf(", ");
+		if (i != n - 1)
+		{
+			printf("%d, ", a[i]);
+		}
+		else
+		{
+			printf("%d", a[i]);
+		}
+		
 	}
 	printf("\n");
 }
