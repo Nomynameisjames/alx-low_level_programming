04aac0dafcc695695b2f5cb472d509cf9f4f36a1
Nomynameisjames <adavaonimisi@gmail.com>
2022-06-15 14:45:57 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,7 +7,7 @@
  **@src: pointer to char
  *Return: concantinated pointer string
  */
-char *_strcat(char *dest, char *src)char *_strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
 	strcat(dest, src);
 	return (dest);
