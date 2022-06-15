333586b55ef91adb5d23b135b4b6482bda409a64
Nomynameisjames <adavaonimisi@gmail.com>
2022-06-15 15:05:34 -0700

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,7 +7,8 @@
  *@n: maximum number of characters to be appended.
  *Return: concantenet string
  */
-char *_strncat(char *dest, char *src, int n);
+
+char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0, j = 0;
 
