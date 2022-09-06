a785255458eae04ac636405a1ca12f7d3b24d2e1
Nomynameisjames <adavaonimisi@gmail.com>
2022-09-05 19:40:00 -0700

    libdynamic

--- 0x18-dynamic_libraries/0-main.c
+++ 0x18-dynamic_libraries/0-main.c
@@ -6,16 +6,6 @@
  *
  * Return: Always EXIT_SUCCESS.
  */
-
-int _strlen(char *s)
-{
-	int count, inc;
-	inc = 0;
-	for (count = 0; s[count] != '\0'; count++)
-	inc++;
-
-	return (inc);
-}
 int main(void)
 {
 	printf("%d\n", _strlen("My Dyn Lib"));
