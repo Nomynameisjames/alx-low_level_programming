2f87e471c1b4fda3fd954ed13780c15609fcf6ca
Nomynameisjames <adavaonimisi@gmail.com>
2023-01-04 13:17:47 -0800

    linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -7,17 +7,17 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-	unsigned int i = 0;
+	size_t  i = 0;
 	if (!array)
-		return -1;
+		return (-1);
 	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%d]\n", i);
-		if (array[i] == value)
+		printf("Value checked array[%d] = [%d]\n", (int)i, array[i]);
+		if (value == array[i])
 		{	
 			return (i);
 		}
 	}
 
-	return -1;
+	return (-1);
 }
